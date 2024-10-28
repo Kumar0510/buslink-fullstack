c6634313143b604ab7f3d631330c201d0ada5a52
Kumar0510 <149935210+Kumar0510@users.noreply.github.com>
2024-10-28 14:48:51 +0530

    Update About.jsx

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -6,7 +6,7 @@ function About() {
     
     <div className='text-center m-10 card mx-auto'>
       <div className="card-body">
-        Welcome to BusLink – your one-stop platform for convenient and hassle-free bus pass management. We provide students with seamless options for bus pass registration and easy renewals for 30 or 90 days. Our mission is to make daily commutes simpler, saving you time and ensuring reliable access to transit passes. With BPA, managing your travel is now just a few clicks away. Join us in making your journey smarter, faster, and more accessible!
+        Welcome to BusLink – your one-stop platform for convenient and hassle-free bus pass management. We provide students with seamless options for bus pass registration and easy renewals for 30 or 90 days. Our mission is to make daily commutes simpler, saving you time and ensuring reliable access to transit passes. With BusLink, managing your travel is now just a few clicks away. Join us in making your journey smarter, faster, and more accessible!
       </div>
     </div>
 
