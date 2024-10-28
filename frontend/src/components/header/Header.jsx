b3adf6090185c03d44ede80899920a8e93befcee
Kumar0510 <149935210+Kumar0510@users.noreply.github.com>
2024-10-28 14:23:28 +0530

    Update Header.jsx

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
     <div>
       <div className='d-flex flex-wrap justify-content-around p-1 main'>
         <div className="productHeader">
-          <img src="../../assets/logo.png" alt="" className = "logoheader"/>
+          <img src="https://in.images.search.yahoo.com/yhs/search?p=bus+logo+images&fr=yhs-sz-002&type=type80260-1822412816&hspart=sz&hsimp=yhs-002&imgurl=https%3A%2F%2Fimg.freepik.com%2Ffree-vector%2Fbus-logo-abstract_7315-17.jpg%3Fw%3D2000#id=7&iurl=https%3A%2F%2Fimg.freepik.com%2Ffree-vector%2Fbus-logo-abstract_7315-17.jpg%3Fw%3D2000&action=click" alt="" className = "logoheader"/>
           <h3 className='companyname text-white'>BusLink</h3>
         </div>
         <ul className='nav justify-content-end fs-4 navMain'>
